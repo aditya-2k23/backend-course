6855172c2b4a936e34e0d847568ff3028151068b
aditya-2k23 <holaaditya123@gmail.com>
2024-12-30 16:21:29 +0530

    Implement authentication middleware and CRUD operations for todos

--- project/src/routes/todoRoutes.js
+++ project/src/routes/todoRoutes.js
@@ -15,12 +15,43 @@ router.get("/", (req, res) => {
 });
 
 // Create a new todo
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  const { task } = req.body;
+  const insertTodo = db.prepare(`
+        INSERT INTO todos (user_id, task)
+        VALUES (?, ?)
+    `);
+  const result = insertTodo.run(req.userId, task); // *
+
+  res.json({ id: result.lastInsertRowid, task, completed: 0 });
+});
 
 // Update a todo
-router.put("/:id", (req, res) => {}); // ?: The ":id" is used to capture the id of the todo that we want to update
+router.put("/:id", (req, res) => {
+  const { completed } = req.body;
+  const { id } = req.params; // ? req.params is an object containing properties mapped to the named route “parameters”. For example, if you have the route /user/:name, then the “name” property is available as req.params.name. This object defaults to {}.
+  const { page } = req.query; // ? req.query is an object containing a property for each query string parameter in the route. If there is no query string, it is an empty object: {}. For example, if you have the route /search?q=toby, then the “q” property is available as req.query.q. This object defaults to {}.
+
+  const updatedTodo = db.prepare(`
+    UPDATE todos SET completed = ?
+    WHERE id = ?
+  `);
+
+  updatedTodo.run(completed, id); // ? This will update the completed status of the todo with the given id
+
+  res.json({ message: "Todo completed!" });
+}); // ?: The ":id" is used to capture the id of the todo that we want to update
 
 // Delete a todo
-router.delete("/:id", (req, res) => {}); // ?: Same as above, the ":id" is used to capture the id of the todo that we want to delete
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deleteTodo = db.prepare(`
+    DELETE FROM todos
+    WHERE id = ? AND user_id = ?
+  `);
+
+  deleteTodo.run(id, req.userId); // ? This will delete the todo with the given id
+  res.send({ message: "Todo deleted!" });
+}); // ?: Same as above, the ":id" is used to capture the id of the todo that we want to delete
 
 export default router;
